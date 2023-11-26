1efb1e49b44d66cac484930afd09ebb9f2dcefc7
Varfolomeevaa <v.varfolomeeva@g.nsu.ru>
2023-11-26 15:51:21 +0700

    123

--- main.py
+++ main.py
@@ -5,14 +5,45 @@ def square(n):
     for _ in range(4):
         turtle.fd(n)
         turtle.rt(90)
+
+
 def rec_square(n):
     if n <= 1:
         return None
     square(n)
     turtle.rt(10)
     turtle.penup()
-    turtle.fd(0.1*n)
+    turtle.fd(0.1 * n)
     turtle.pendown()
-    return rec_square(0.9*n)
+    return rec_square(0.9 * n)
 
-rec_square(100)
+
+# rec_square(100)
+
+def color_tree(depth, size):
+    turtle.colormode(255)
+    cg = 255 - int(depth * (250 / 6)) % 255
+    turtle.color(0, cg, 0)
+    if depth == 0:
+        turtle.fd(size)
+    else:
+        turtle.fd(size)
+        turtle.rt(45)
+        color_tree(depth - 1, size * 0.6)
+        turtle.penup()
+        turtle.bk(size * 0.6)
+        turtle.pendown()
+        turtle.lt(90)
+        color_tree(depth - 1, size * 0.6)
+        turtle.penup()
+        turtle.bk(size * 0.6)
+        turtle.pendown()
+        turtle.rt(45)
+
+
+'''
+turtle.left(90)
+color_tree(4, 100)
+turtle.done()
+'''
+
